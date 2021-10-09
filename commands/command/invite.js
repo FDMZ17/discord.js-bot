45fd0894e6bb14e0d692b79246ec90e53572a873
FDMZ17 <85776604+FDMZ17@users.noreply.github.com>
2021-10-09 13:51:47 +0700

    ✨ Update [v2.0]

--- commands/command/invite.js
+++ commands/command/invite.js
@@ -4,7 +4,6 @@ module.exports = {
   name: "invite",
   run: async (client, message, args) => {
     const embed = new discord.MessageEmbed()
-
             .setTitle("Invite:")
             .setDescription(`[Click to invite me](https://discord.com/api/oauth2/authorize?client_id=${client.setting.invite.botid}&permissions=3669056&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D${client.setting.invite.botid}%26permissions%3D268435472%26scope%3Dbot&scope=bot)
             
