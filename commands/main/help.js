ad00f7cd95ad690903eef6c8fb8be0413042b8f0
FDMZ17 <85776604+FDMZ17@users.noreply.github.com>
2021-10-09 17:16:36 +0700

    ✨ Update [v2.0]

--- commands/main/help.js
+++ commands/main/help.js
@@ -4,21 +4,22 @@ module.exports = {
     name: "help",
     description: "help panel",
     async run (client, message, args){
-        const main = new Discord.MessageEmbed()
+        const page1 = new Discord.MessageEmbed()
         .setTitle('Main')
         .addField('help', 'Show this menu')
         .addField('ping', 'Ping between bot and server')
         .addField('prefix', 'Set your bot prefix *per server')
         .setColor('#2e3137')
         .setTimestamp()
-        const level = new Discord.MessageEmbed()
-        .setTitle('level')
-        .addField('level', 'Show your current level')
+        const page2 = new Discord.MessageEmbed()
+        .setTitle('Your cmd')
+        .addField('cmd', 'desc')
+        .addField('cmd', 'desc')
         .setColor('#2e3137')
         .setTimestamp()
         const pages = [
-                main,
-                level
+                page1,
+                page2
         ]
         const emojiList = ["◀️", "▶️"];
         const timeout = '120000';
--- index.js
+++ index.js
@@ -52,36 +52,6 @@ client.on('ready', () => {
 	console.log('Online.');
 });
 
-client.on('message', async message => {
-    let prefix = await db.get(`prefix_${message.guild.id}`)
-    if(!message.guild) return;
-    if(message.author.bot) return;
-    xp(message)
-    const args = message.content.slice(prefix.length).trim().split(/ +/g)
-    const command = args.shift().toLowerCase()
-
-    const cmd = client.commands.get(command)
-    if(cmd) cmd.run(client, message, args)
-})
-
-async function xp(message) {
-    let prefix = await db.get(`prefix_${message.guild.id}`)
-    if(prefix === null) prefix = '!'
-    if(message.content.startsWith(prefix)) return;
-    const randomNumber = Math.floor(Math.random() * 1) + 5
-    db.add(`guild_${message.guild.id}_xp_${message.author.id}`, randomNumber)
-    db.add(`guild_${message.guild.id}_xptotal_${message.guild.id}`, randomNumber)
-    var level = db.get(`guild_${message.guild.id}_rank_${message.author.id}`) || 1
-    var level = db.get(`guild_${message.guild.id}_level_${message.author.id}`) || 1
-    var xp = db.get(`guild_${message.guild.id}_xp_${message.author.id}`)
-    var xpNeeded = level * 500
-    if(xpNeeded < xp) {
-        var newLevel = db.add(`guild_${message.guild.id}_level_${message.author.id}`, 1)
-        db.subtract(`guild_${message.guild.id}_xp_${message.author.id}`, xpNeeded)
-        message.channel.send(`Congrats ${message.author}, you levelled up to Level ${newLevel}`)
-    }
-}
-
 
 
 
