6ff699681edcd847c865b564a0c9172dae2d07c4
FDMZ17 <85776604+FDMZ17@users.noreply.github.com>
2021-10-09 10:10:36 +0700

    editing setting.js

--- setting.js
+++ setting.js
@@ -2,7 +2,7 @@ module.exports = {
     bot: {
         token: 'xxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxxx',
         prefix: '!',
-        botid: '0000000000000000',
+        invitebotid: '0000000000000000',
         status: '!help',
         statustype: 'WATCHING'
     },
